771552a8d5007d8aa6d3527d229da40d6029a1d8
Linh Hoàng <redtonlinh@gmail.com>
2022-09-14 01:30:33 +0700

    update lab4

---
+++ Thongtinsinhvien/src/BookingRoom/Date.java
@@ -0,0 +1,14 @@
+package BookingRoom;
+
+public class Date {
+    byte day;
+    byte month;
+    short year;
+
+    public Date() {
+        day = 0;
+        month = 0;
+        year = 0;
+    }
+
+}
---
+++ Thongtinsinhvien/src/BookingRoom/Time.java
@@ -0,0 +1,12 @@
+package BookingRoom;
+
+public class Time {
+    byte hours;
+    byte minutes;
+
+    public Time() {
+        hours = 0;
+        minutes = 0;
+    }
+
+}
